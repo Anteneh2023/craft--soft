e88cbed83701f5f577c73169cdb653fb14326be2
Anteneh2023 <126181478+Anteneh2023@users.noreply.github.com>
2023-02-28 20:30:44 -0500

    commit this file

---
+++ MyJavasimple/src/Comm/CD/Javasimpel.java
@@ -0,0 +1,12 @@
+package Comm.CD;
+
+public class Javasimpel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My first word");
+System.out.println("My first word");
+System.out.println("My first word");
+	}
+
+}
