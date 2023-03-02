a96ff4e224689720125843df3b761f693a06a263
Anteneh2023 <126181478+Anteneh2023@users.noreply.github.com>
2023-03-01 20:19:32 -0500

    My fist branch in Java.

--- MyJavasimple/src/Comm/CD/Javasimpel.java
+++ MyJavasimple/src/Comm/CD/Javasimpel.java
@@ -4,9 +4,9 @@ public class Javasimpel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("My first word");
-System.out.println("My first word");
-System.out.println("My first word");
+System.out.println("My first branch");
+System.out.println("My first branch");
+System.out.println("My first branch");
 	}
 
 }
